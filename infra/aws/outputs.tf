aeceb259875ca7d17863770a96b7cacba4cf7d46
im-mk <45291252+im-mk@users.noreply.github.com>
2024-08-23 00:39:07 +0100

    fixed ecr name

--- infra/aws/outputs.tf
+++ infra/aws/outputs.tf
@@ -1,4 +1,4 @@
 output "repository_url" {
   description = "The URL of the created ECR repository."
-  value       = aws_ecr_repository.app_repo.repository_url
+  value       = aws_ecr_repository.go_app_ecr.repository_url
 }
